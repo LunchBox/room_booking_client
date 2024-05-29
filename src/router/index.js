ce3c2b864a9acff73a531e25b214867fffee62ad
daniel cheang <daniel@cpttm.org.mo>
2024-05-29 10:22:43 +0800

    add sign in / up page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/sign_up",
+      name: "sign_up",
+      component: () => import("../views/SignUp.vue"),
+    },
+    {
+      path: "/sign_in",
+      name: "sign_in",
+      component: () => import("../views/SignIn.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/SignIn.vue
@@ -0,0 +1,19 @@
+<template>
+	<h2>Sign In</h2>
+
+	<form>
+		<div>
+			<label>
+				<span>Name</span>
+				<input type="text">
+			</label>
+			<label>
+				<span>Password</span>
+				<input type="password">
+			</label>
+			<div>
+				<button>Submit</button>
+			</div>
+		</div>
+	</form>
+</template>
